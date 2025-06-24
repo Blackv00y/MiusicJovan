22f05673c035b85ad4a1df9c0e51804cb494ba16
Blackv00y <yordywang47@gmail.com>
2025-06-24 12:33:05 -0600

    Subida inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+// Reemplaza con tu Client ID
+const clientId = '60abfec850fb4ee18f710c9255ed19db';
+const redirectUri = 'https://localhost.com/callback';
+const scopes = 'user-read-private user-read-email';
+
+function login() {
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
+  window.location.href = authUrl;
+}
