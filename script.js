2f68b4be118d878d9d580665253e77c76a3f7991
Blackv00y <yordywang47@gmail.com>
2025-06-25 01:59:51 -0600

    Versión con YouTube y modo oscuro

--- script.js
+++
@@ -1,9 +0,0 @@
-// Reemplaza con tu Client ID
-const clientId = '60abfec850fb4ee18f710c9255ed19db';
-const redirectUri = 'https://blackv00y.github.io/MiusicJovan/callback.html';
-const scopes = 'user-read-private user-read-email';
-
-function login() {
-  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
-  window.location.href = authUrl;
-}
